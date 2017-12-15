c84081b20401c0fc213e8cbe408d05659ad7619b
unknown <N426>
2017-12-15 13:59:11 +0800

    'a'

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2017-10-08 20:23:02
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-10-08 20:26:39
+*/
+window.onload=function(){
+	var text=document.querySelector('.text')
+	text.animate({width:400,height:100}, 3000);
+}
